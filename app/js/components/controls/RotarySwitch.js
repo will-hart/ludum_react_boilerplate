e40bc098fcb09975dd6d0d67105b23b90222f52a
Will Hart <hart.wl@gmail.com>
2015-12-13 10:18:42 +0000

    Add intro thump sound effect

--- app/js/components/controls/RotarySwitch.js
+++ app/js/components/controls/RotarySwitch.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import AudioPlayer from "../../services/AudioPlayer";
 
 
--- app/js/components/routes/Intro.js
+++ app/js/components/routes/Intro.js
@@ -1,12 +1,35 @@
-import React from "react"
-import FadeInText from "../common/FadeInText";
-
+import React from "react";
 import router from "../../router";
+
+import AudioPlayer from "../../services/AudioPlayer";
+import FadeInText from "../common/FadeInText";
 
 
 class Intro extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
+
+		this.audio = new AudioPlayer();
+		this.audioTimeouts = [];
+	}
+
+	componentDidMount() {
+		this.audioTimeouts.push(this._timedAudio(13900));
+		this.audioTimeouts.push(this._timedAudio(14900));
+		this.audioTimeouts.push(this._timedAudio(15900));
+		this.audioTimeouts.push(this._timedAudio(16900));
+		this.audioTimeouts.push(this._timedAudio(17900));
+		this.audioTimeouts.push(this._timedAudio(20400));
+	}
+
+	componentWillUnmount() {
+		this.audioTimeouts.forEach((t) => {
+			clearTimeout(t);
+		});
+	}
+
+	_timedAudio(delay) {
+		return setTimeout(() => {this.audio.play("thump");}, delay);
 	}
 
 	_startGame(e) {
@@ -24,12 +47,12 @@ class Intro extends React.Component {
 					<FadeInText text="without a parachute." delay={8000} />
 					<FadeInText text="Its fine, I'm sure you can learn on the job." delay={10000} />
 					<FadeInText text="Oh, and..." delay={12000} />
-					<FadeInText text="don't" delay={14000} />
-					<FadeInText text="let " delay={14400} />
-					<FadeInText text="the" delay={14800} />
-					<FadeInText text="crops" delay={15200} />
-					<FadeInText text="die" delay={15600} />
-					<FadeInText text="(or else)" delay={18000} />
+					<FadeInText text="don't" delay={13500} />
+					<FadeInText text="let " delay={14500} />
+					<FadeInText text="the" delay={15500} />
+					<FadeInText text="crops" delay={16500} />
+					<FadeInText text="die" delay={17500} />
+					<FadeInText text="(or else)" delay={20000} />
 				</div>
 
 				<FadeInText text={
