5546cfcd83af15673f7cf46d8ea07c5c77b1bfa9
Will Hart <hart.wl@gmail.com>
2016-04-17 13:02:25 +1000

    Add legend for controls

--- src/js/components/PlayerKeys/PlayerKeys.js
+++ src/js/components/PlayerKeys/PlayerKeys.js
@@ -6,6 +6,9 @@ export default class PlayerKeys extends React.Component {
   render() {
     return (
       <div className="player-keys">
+
+        <p className="legend">CONTROLS</p>
+
         <div className="player-key-swatch key-0">A</div>
 
         <div className="player-key-swatch key-1">S</div>
